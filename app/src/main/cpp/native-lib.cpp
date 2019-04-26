fd4540ea652b3eb75cbfc4b092ec500ec12d3edf
qi.wang.x <qi.wang.x@cloudminds.com>
2019-04-26 19:57:35 +0800

    更新

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -11,6 +11,7 @@
 extern "C" {
 #include "libavformat/avformat.h"
 #include "libswscale/swscale.h"
+#include <libswresample/swresample.h>
 }
 
 #define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO,"jason",FORMAT,##__VA_ARGS__);
@@ -89,7 +90,6 @@ JNIEXPORT void JNICALL Java_com_example_wqllj_ffmpegdemo_MainActivity_play
 
     //封装格式上下文
     AVFormatContext *pFormatCtx = avformat_alloc_context();
-    check(env);
     //2.打开输入视频文件
     if (avformat_open_input(&pFormatCtx, input_cstr, NULL, NULL) != 0) {
         LOGE("%s", "打开输入视频文件失败");
@@ -101,47 +101,69 @@ JNIEXPORT void JNICALL Java_com_example_wqllj_ffmpegdemo_MainActivity_play
         return;
     }
     LOGI("视频格式 %s", pFormatCtx->iformat->name)
-    LOGI("视频时长 %d", pFormatCtx->duration / 1000000)
+    LOGI("视频时长 %d", (int)(pFormatCtx->duration / AV_TIME_BASE))
 
     //视频解码，需要找到视频对应的AVStream所在pFormatCtx->streams的索引位置
     int video_stream_idx = -1;
+    int audio_stream_idx = -1;
     int i = 0;
     for (; i < pFormatCtx->nb_streams; i++) {
         //根据类型判断，是否是视频流
         if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
             video_stream_idx = i;
-            break;
+        }else if(pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO){
+            audio_stream_idx = i;
         }
     }
-    LOGI("video_stream_idx =  %d", video_stream_idx)
-    check(env);
+    LOGI("video_stream_idx =  %d audio_stream_idx = %d", video_stream_idx,audio_stream_idx)
 
     //4.获取视频解码器
     AVCodecContext *pCodeCtx = pFormatCtx->streams[video_stream_idx]->codec;
-    check(env);
     AVCodec *pCodec = avcodec_find_decoder(pCodeCtx->codec_id);
-    check(env);
     if (pCodec == NULL) {
-        LOGE("%s", "无法解码");
+        LOGE("%s", "视频无法解码");
         return;
     }
     LOGI("视频宽高  %d  %d", pCodeCtx->width, pCodeCtx->height)
     LOGI("视频帧  %d", pCodeCtx->bit_rate)
     LOGI("解码器名称 %s", pCodec->name)
+    AVCodecContext *audioCodeCtx = pFormatCtx->streams[audio_stream_idx]->codec;
+    AVCodec *audioCodec = avcodec_find_decoder(audioCodeCtx->codec_id);
+    LOGI("音频解码器名称 %s", audioCodec->name)
+    if(audioCodec == NULL){
+        LOGE("%s", "音频无法解码");
+        return;
+    }
     //5.打开解码器
     if (avcodec_open2(pCodeCtx, pCodec, NULL) < 0) {
-        LOGE("%s", "解码器无法打开");
+        LOGE("%s", "视频解码器无法打开");
+        return;
+    }
+    if (avcodec_open2(audioCodeCtx, audioCodec, NULL) < 0) {
+        LOGE("%s", "音频解码器无法打开");
         return;
     }
 
     //编码数据
     AVPacket *packet = (AVPacket *) av_malloc(sizeof(AVPacket));
-    check(env);
     //像素数据（解码数据）
     AVFrame *yuv_frame = av_frame_alloc();
-    check(env);
     AVFrame *rgb_frame = av_frame_alloc();
-    check(env);
+    AVFrame *audio_frame = av_frame_alloc();
+    //frame->16bit 44100 PCM 统一音频采样格式与采样率
+    SwrContext *swrCtx = swr_alloc();
+    enum AVSampleFormat inSsmpleFmt = audioCodeCtx->sample_fmt;
+    enum AVSampleFormat outSsmpleFmt = AV_SAMPLE_FMT_S16;
+    int inSampleRate = audioCodeCtx->sample_rate;
+    int outSampleRate = inSampleRate;
+    //获取输入的声道布局
+    //根据声道个数获取默认的声道布局（2个声道，默认立体声stereo）
+    //av_get_default_channel_layout(codecCtx->channels);
+    uint64_t in_chanel_layout = audioCodeCtx->channel_layout;
+    uint64_t out_chanel_layout = AV_CH_LAYOUT_STEREO;
+    swr_alloc_set_opts(swrCtx,out_chanel_layout,
+    outSsmpleFmt,outSampleRate,in_chanel_layout,inSsmpleFmt,
+    inSampleRate,0,NULL);
     //native绘制
     //窗体
     ANativeWindow *nativeWindow = ANativeWindow_fromSurface(env, surface);
@@ -155,29 +177,40 @@ JNIEXPORT void JNICALL Java_com_example_wqllj_ffmpegdemo_MainActivity_play
                     pCodeCtx->width, pCodeCtx->height, pCodeCtx->pix_fmt,
                     width, height, AV_PIX_FMT_RGBA,
                     SWS_BILINEAR, NULL, NULL, NULL);
-    int len, got_frame, framecount = 0;
+    swr_init(swrCtx);
+//    输出文件
+    // /storage/emulated/0/kugou/mv/光年之外-art--G.E.M.邓紫棋--art-14a694dd09e9f655f0485b4d06a1ac4f.mp4
+    char path[100];
+    std::string inPath(input_cstr);
+    sprintf(path,"/storage/emulated/0/kugou/mv/%s%s",inPath.substr(inPath.find_last_of("/")+1,inPath.find_last_of(".")-inPath.find_last_of("/")).c_str(),audioCodec->name);
+    LOGI("音频保存地址：%d  %d   %s",inPath.find_last_of("/")+1,inPath.find_last_of("."),path)
+    FILE *file = fopen(path,"wb");
+    //16bit 44100 PCM 数据
+    uint8_t *out_buffer = (uint8_t *)av_malloc(2*44100);
+    int ret, got_frame, framecount = 0;
     //6.一阵一阵读取压缩的视频数据AVPacket
     if (nativeWindow != NULL) {
         while (av_read_frame(pFormatCtx, packet) >= 0) {
-
-            //解码AVPacket->AVFrame
-            len = avcodec_decode_video2(pCodeCtx, yuv_frame, &got_frame, packet);
-            //Zero if no frame could be decompressed
-            //非零，正在解码
-            if (got_frame) {
-                LOGI("解码%d帧", framecount++);
+            if(packet->stream_index == video_stream_idx) {
+                //解码AVPacket->AVFrame
+                ret = avcodec_decode_video2(pCodeCtx, yuv_frame, &got_frame, packet);
+                //Zero if no frame could be decompressed
+                //非零，正在解码
+                if (got_frame) {
+                    LOGI("解码%d帧", framecount++);
 //                LOGI("宽 %d 高 %d", nativeWindow, +pCodeCtx->height);
-                //lock
-                //设置缓冲区的属性（宽、高、像素格式）
-                ANativeWindow_setBuffersGeometry(nativeWindow, width, height,
-                                                 WINDOW_FORMAT_RGBA_8888);
-                ANativeWindow_lock(nativeWindow, &outBuffer, NULL);
-                //设置rgb_frame的属性（像素格式、宽高）和缓冲区
-                //rgb_frame缓冲区与outBuffer.bits是同一块内存
-                avpicture_fill((AVPicture *) rgb_frame, (const uint8_t *) outBuffer.bits,
-                               PIX_FMT_RGBA,
-                               width, height);
-                LOGI("outBuffer.stride = %d  %d   %d",outBuffer.stride,outBuffer.width,outBuffer.height);
+                    //lock
+                    //设置缓冲区的属性（宽、高、像素格式）
+                    ANativeWindow_setBuffersGeometry(nativeWindow, width, height,
+                                                     WINDOW_FORMAT_RGBA_8888);
+                    ANativeWindow_lock(nativeWindow, &outBuffer, NULL);
+                    //设置rgb_frame的属性（像素格式、宽高）和缓冲区
+                    //rgb_frame缓冲区与outBuffer.bits是同一块内存
+                    avpicture_fill((AVPicture *) rgb_frame, (const uint8_t *) outBuffer.bits,
+                                   PIX_FMT_RGBA,
+                                   width, height);
+                    LOGI("outBuffer.stride = %d  %d   %d", outBuffer.stride, outBuffer.width,
+                         outBuffer.height);
 //                if (!yuv_scale_frame->linesize[0]) {
 //                    //只有指定了AVFrame的像素格式、画面大小才能真正分配内存
 //                    //缓冲区分配内存
@@ -189,13 +222,16 @@ JNIEXPORT void JNICALL Java_com_example_wqllj_ffmpegdemo_MainActivity_play
 //                                   width, height);
 //
 //                }
-                rgb_frame->linesize[0] = outBuffer.stride*2;
-                LOGI("rgb_frame宽高1 %d %d %d  %d",yuv_frame->height ,rgb_frame->width,rgb_frame->height,rgb_frame->linesize[0])
-                int h = sws_scale(sws_ctx,
-                          (const uint8_t *const *) yuv_frame->data, yuv_frame->linesize, 0, yuv_frame->height,
-                          rgb_frame->data, rgb_frame->linesize);
+                    rgb_frame->linesize[0] = outBuffer.stride * 2;
+                    LOGI("rgb_frame宽高1 %d %d %d  %d", yuv_frame->height, rgb_frame->width,
+                         rgb_frame->height, rgb_frame->linesize[0])
+                    int h = sws_scale(sws_ctx,
+                                      (const uint8_t *const *) yuv_frame->data, yuv_frame->linesize,
+                                      0, yuv_frame->height,
+                                      rgb_frame->data, rgb_frame->linesize);
 
-                LOGI("rgb_frame宽高2 %d   %d  %d %d  %d",yuv_frame->height ,h,rgb_frame->width,rgb_frame->height,rgb_frame->linesize[0])
+                    LOGI("rgb_frame宽高2 %d   %d  %d %d  %d", yuv_frame->height, h, rgb_frame->width,
+                         rgb_frame->height, rgb_frame->linesize[0])
 
 
 //                int h=libyuv::I420ToARGB(yuv_frame->data[0], yuv_frame->linesize[0],
@@ -206,23 +242,30 @@ JNIEXPORT void JNICALL Java_com_example_wqllj_ffmpegdemo_MainActivity_play
 //                                LOGI("rgb_frame宽高2 %d   %d  %d %d ", yuv_frame->height, h,
 //                                     rgb_frame->width, rgb_frame->height)
 
-                if (check(env)) {
-                    break;
-                }
+                    if (check(env)) {
+                        break;
+                    }
 
-                //unlock
-                ANativeWindow_unlockAndPost(nativeWindow);
-                if (check(env)) {
-                    break;
-                }
-                usleep(1000 * 16);
+                    //unlock
+                    ANativeWindow_unlockAndPost(nativeWindow);
+                    if (check(env)) {
+                        break;
+                    }
+                    usleep(1000 * 16);
 
+                }
+            }else if(packet->stream_index == audio_stream_idx){
+                ret = avcodec_decode_audio4(audioCodeCtx,audio_frame,&got_frame,packet);
+                if(ret<0){
+                    LOGI("音频解码完成")
+                }
+                if(got_frame>0){
+                    LOGI("解码 %d",framecount++);
+                    fwrite(packet->data,1,packet->size,file);
+                }
             }
 
             av_free_packet(packet);
-            if (check(env)) {
-                break;
-            }
         }
     } else {
         LOGI("nativeWindow == NULL");
@@ -230,6 +273,7 @@ JNIEXPORT void JNICALL Java_com_example_wqllj_ffmpegdemo_MainActivity_play
     if (nativeWindow) {
         ANativeWindow_release(nativeWindow);
     }
+    swr_free(&swrCtx);
     sws_freeContext(sws_ctx);
     check(env);
     av_frame_free(&yuv_frame);
